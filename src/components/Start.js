f670646c212bc24c9ea5be33aa7510902ff259d5
Bita <bita.janzadeh@hotmail.com>
2022-03-10 22:08:39 -0500

    : Define state in Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 
 export default class Start extends Component {
+  state = {
+    step: 1,
+    gender: "",
+    age: "",
+    meningitis: false,
+    workCondition: false,
+    publicSafety: false,
+  };
+
   render() {
-    return <div>Welcome to the vaccine quiz</div>;
+    return (
+      <div>
+        Welcome to the vaccine quiz Welcome to the vaccine quiz Welcome to the
+        vaccine quiz Welcome to the vaccine quiz
+      </div>
+    );
   }
 }
